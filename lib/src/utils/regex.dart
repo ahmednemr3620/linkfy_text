7463adac0a5068bbfcd92d2095e5eba23648064a
Ahmed Hosni <145866429+ahmednemr3620@users.noreply.github.com>
2024-09-22 02:04:47 +0300

    Update regex.dart

--- lib/src/utils/regex.dart
+++ lib/src/utils/regex.dart
@@ -2,13 +2,89 @@ import 'package:linkfy_text/src/enum.dart';
 
 // String urlRegExp =
 
+///Regular Expression values taken from https://github.com/santa112358/detectable_text_field
+
+const _symbols = '·・ー_';
+
+const _numbers = '0-9０-９';
+
+const _englishLetters = 'a-zA-Zａ-ｚＡ-Ｚ';
+
+const _japaneseLetters = 'ぁ-んァ-ン一-龠';
+
+const _koreanLetters = '\u1100-\u11FF\uAC00-\uD7A3';
+
+const _spanishLetters = 'áàãâéêíóôõúüçÁÀÃÂÉÊÍÓÔÕÚÜÇ';
+
+const _arabicLetters = '\u0621-\u064A';
+
+const _thaiLetters = '\u0E00-\u0E7F';
+
+const _turkishLetters = 'ğüşıöçĞÜŞİÖÇ';
+
+const _vietnameseLetters =
+    'AĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴAĂÂÁẮẤÀẰẦẢẲẨÃẴẪẠẶẬĐEÊÉẾÈỀẺỂẼỄẸỆIÍÌỈĨỊOÔƠÓỐỚÒỒỜỎỔỞÕỖỠỌỘỢUƯÚỨÙỪỦỬŨỮỤỰYÝỲỶỸỴ';
+final _vietnameseLettersLowerCase = _vietnameseLetters.toLowerCase();
+
+const _persianLetters = "\u0600-\u06FF";
+
+final detectionContentLetters = _symbols +
+    _numbers +
+    _englishLetters +
+    _japaneseLetters +
+    _koreanLetters +
+    _spanishLetters +
+    _arabicLetters +
+    _thaiLetters +
+    _vietnameseLetters +
+    _vietnameseLettersLowerCase +
+    _persianLetters +
+    _turkishLetters;
+
+// const urlRegexContent = "((http|https)://)(www.)?" +
+//     "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+//     "{2,256}\\.[a-z]" +
+//     "{2,6}\\b([-a-zA-Z0-9@:%" +
+//     "._\\+~#?&//=]*)";
+
+// Thanks to @Bhupesh-V for contribution.
+const urlRegexContent = "((http|https)://)(www.)?" +
+    "[a-zA-Z0-9@:%._\\+~#?&//=-]" +
+    "{2,256}\\.[a-z]" +
+    "{2,6}\\b([-a-zA-Z0-9@:%" +
+    "._\\+~#?&//=]*)";
+
+/// Regular expression to extract hashtag
+///
+/// Supports English, Japanese, Korean, Spanish, Arabic, and Thai
+final hashTagReg = RegExp(
+  "(?!\\n)(?:^|\\s)(#([$detectionContentLetters]+))",
+  multiLine: true,
+);
+
+final atSignRegExp = RegExp(
+  "(?!\\n)(?:^|\\s)([@]([$detectionContentLetters]+))",
+  multiLine: true,
+);
+
+final urlRegex = RegExp(
+  urlRegexContent,
+  multiLine: true,
+);
+
+final emojiRegex = RegExp(
+  r'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])',
+);
+
+
+
 // url regex that accept https, http, www
 String urlRegExp =
     r'((https?://)?(www\.)?[a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))';
 
-String hashtagRegExp = r'#[a-zA-Z\u00C0-\u01B4\w_\u1EA0-\u1EF9!$%^&]{1,}(?=\s|$)';
+String hashtagRegExp = hashTagReg.pattern;
 
-String userTagRegExp = r'@[a-zA-Z\u00C0-\u01B4\w_\u1EA0-\u1EF9!$%^&]{1,}(?=\s|$)';
+String userTagRegExp = r'(?<![\w@])@([\w@]+(?:[.!][\w@]+)*)';
 String phoneRegExp =
     r'\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*';
 String emailRegExp =
@@ -59,13 +135,13 @@ LinkType getMatchedType(String match) {
   late LinkType type;
   if (RegExp(emailRegExp).hasMatch(match)) {
     type = LinkType.email;
-  } else if (RegExp(urlRegExp).hasMatch(match)) {
-    type = LinkType.url;
-  }else if (RegExp(phoneRegExp).hasMatch(match)) {
+  } else if (RegExp(phoneRegExp).hasMatch(match)) {
     type = LinkType.phone;
   } else if (RegExp(userTagRegExp).hasMatch(match)) {
     type = LinkType.userTag;
-  }  else if (RegExp(hashtagRegExp).hasMatch(match)) {
+  } else if (RegExp(urlRegExp).hasMatch(match)) {
+    type = LinkType.url;
+  } else if (hashTagReg.hasMatch(match)) {
     type = LinkType.hashTag;
   }
   return type;
