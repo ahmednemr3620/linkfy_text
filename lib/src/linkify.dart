716d8896b9bc59b142e27eaf01583e48d51e0539
Ahmed Hosni <145866429+ahmednemr3620@users.noreply.github.com>
2024-11-26 01:49:25 +0200

    Update linkify.dart

--- lib/src/linkify.dart
+++ lib/src/linkify.dart
@@ -405,7 +405,7 @@ TextSpan _linkify({
       spans.add(
         TextSpan(
           text: link.value,
-          style:getCustomTextStyle(link)?? customLinkStyles?[link.type] ?? linkStyle,
+          style:getCustomTextStyle?.call(link)?? customLinkStyles?[link.type] ?? linkStyle,
           recognizer: TapGestureRecognizer()
             ..onTap = () {
               if (onTap != null) onTap(link);
